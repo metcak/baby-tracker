7166a34c5f822fb536bc73923fb80477bdd06c72
Metin Cakmak <metin_cakmak@gmx.de>
2023-11-29 15:26:03 +0100

    Add buttons

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Button from './src/components/Button';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Inter-Black', fontSize: 30 }}>Lara Tracker</Text>
+      <View style={styles.mainContainer}>
+        <Button iconName="baby-bottle-outline" label="Feeding" />
+        <Button iconName="sleep" label="Sleeping" />
+        <Button iconName="thermometer" label="Temperature" />
+        <Button iconName="water-outline" label="Pee" />
+        <Button iconName="emoticon-poop" label="Poop" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +24,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 60,
+  },
+  mainContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+    padding: 30,
+    gap: 20,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+    padding: 30,
+    gap: 20,
   },
 });
